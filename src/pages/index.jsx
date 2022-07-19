8b2665a9613987f785b945c10f07f8c0cdc31bd2
basitol <basitolaitan27@gmail.com>
2022-07-19 23:03:20 +0100

    added: export for restaurant page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,2 +1,4 @@
 export { default as Home } from "./Home/Home";
 export { default as UserRegister } from "./signup/user";
+export { default as Restaurants } from "./Restaurant/Restaurants";
+export { default as RestaurantMenu } from "./Restaurant/RestaurantMenu";
