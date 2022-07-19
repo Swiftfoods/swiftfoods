e82ba2afddb4069efbad8e835322ae4e859c28e6
basitol <basitolaitan27@gmail.com>
2022-07-19 22:52:01 +0100

    build: export fpr component

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Footer } from "./Footer/Footer";
+export { default as Button } from "./Button/Button";
+export { default as ProductCard } from "./Product/ProductCard";
+export { default as Newsletter } from "./NewsLetter/NewsLetter";
+export { default as FileInput } from "./FormElements/CustomInput";
