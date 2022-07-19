6960937b473f2c337be495db946e8e812c215307
basitol <basitolaitan27@gmail.com>
2022-07-19 22:52:53 +0100

    build: custom input component component

---
+++ src/components/FormElements/CustomInput.jsx
@@ -0,0 +1,68 @@
+import "./input.scss";
+
+const FileInput = ({
+  inputId,
+  inputType,
+  inputLabel,
+  prefix,
+  suffix,
+  register,
+  registerId,
+  validation,
+  required,
+  errorMsg,
+  children,
+  className,
+  labelClass,
+  ...rest
+}) => {
+  return (
+    <div
+      controlid={inputId}
+      className={`input-field ${className ? className : ""}`}
+    >
+      <div className={`${labelClass}`}>{inputLabel}</div>
+      <div className={`input-container mt-2.5 ${errorMsg ? "error" : ""}`}>
+        {prefix && (
+          <div className="affix prefix">
+            {/* {inputIcon()} */}
+            {prefix}
+          </div>
+        )}
+
+        {register ? (
+          <input
+            type={inputType}
+            {...register(registerId)}
+            required={required}
+            {...rest}
+          />
+        ) : (
+          <input type={inputType} required={required} {...rest} />
+        )}
+
+        {suffix && (
+          <div className="affix suffix">
+            {/* {inputIcon()} */}
+            {suffix}
+          </div>
+        )}
+
+        {children}
+      </div>
+
+      <div className="input-footer">
+        {errorMsg && (
+          <div
+            className="input- error-msg"
+            dangerouslySetInnerHTML={{
+              __html: errorMsg || "error",
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FileInput;
