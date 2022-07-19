4f33935d97aa31f489f9b1499aa10dc30576ce49
basitol <basitolaitan27@gmail.com>
2022-07-19 22:49:10 +0100

    added: dummy data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const signatureDishes = [
+  {
+    id: 1,
+    name: "Crispy Fried Chicken",
+    price: 40,
+    image: "./images/chicken.png",
+    className: "mr-11",
+  },
+  {
+    id: 2,
+    name: "Jollof Rice",
+    price: 50,
+    image: "./images/jollof.png",
+    className: "mr-11",
+  },
+  {
+    id: 3,
+    name: "Chicken Drumstick",
+    price: 15,
+    image: "./images/jollof.png",
+  },
+];
