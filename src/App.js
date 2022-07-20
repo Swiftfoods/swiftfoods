eacde24cbad1d2a1bea9666f2649043989933877
basitol <basitolaitan27@gmail.com>
2022-07-20 23:03:37 +0100

    added: route for user login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, UserRegister, RestaurantMenu, Restaurants } from "./pages";
+import {
+  Home,
+  UserRegister,
+  RestaurantMenu,
+  Restaurants,
+  UserLogin,
+} from "./pages";
 import { Navbar, Footer } from "./components";
 
 function App() {
   const pathname = window.location.pathname;
   console.log(pathname);
   const noSignUp =
-    "/login" ||
     "/signup" ||
     "/forgot-password" ||
     "/vendorregister" ||
@@ -20,10 +25,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/userregister" element={<UserRegister />} />
+        <Route path="/login" element={<UserLogin />} />
         <Route path="/restaurants" element={<Restaurants />} />
         <Route path="/restaurantmenu" element={<RestaurantMenu />} />
       </Routes>
-      {noSignUp.includes(pathname) ? <Footer /> : <div></div>}
+      {pathname !== "/login" && pathname !== "/userregister" ? (
+        <Footer />
+      ) : (
+        <div></div>
+      )}
     </BrowserRouter>
   );
 }
