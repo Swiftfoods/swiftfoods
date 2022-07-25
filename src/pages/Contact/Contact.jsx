44b3276527ff9ad5f5752055eb1c22817d5348f0
basitol <basitolaitan27@gmail.com>
2022-07-25 01:25:50 +0100

    ...

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Loader } from "../../components";
+
+const Contact = () => {
+  return (
+    <div className="w-full">
+      <Loader />
+    </div>
+  );
+};
+
+export default Contact;
