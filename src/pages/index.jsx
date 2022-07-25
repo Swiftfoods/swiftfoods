a888d2a53c06ad13e81eafac231c4ae7541e82a0
basitol <basitolaitan27@gmail.com>
2022-07-25 01:25:22 +0100

    update page and component export

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,3 +3,5 @@ export { default as UserRegister } from "./signup/UserRegister";
 export { default as UserLogin } from "./signup/UserLogin";
 export { default as Restaurants } from "./Restaurant/Restaurants";
 export { default as RestaurantMenu } from "./Restaurant/RestaurantMenu";
+export { default as Mamaputin } from "./Mamaputin/Mamaputin";
+export { default as Contact } from "./Contact/Contact";
